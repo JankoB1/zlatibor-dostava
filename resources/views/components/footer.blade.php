7c7568dd2304467dffadd1ef9c5c78705fd640fd
Janko Blagojevic <janko.tbbt@gmail.com>
2021-11-10 22:52:54 +0100

    Viber test

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -14,7 +14,7 @@
     <div class="drustvene-mreze">
         <a href="https://www.instagram.com/zlatibordostava/"><img src="{{ asset('images/site/instagram.svg') }}" alt="instagram"></a>
         <a href="https://www.facebook.com/profile.php?id=100072357630235"><img src="{{ asset('images/site/facebook.svg') }}" alt="facebook"></a>
-        <a href="viber://chat/?number=%2B+381611609716"><img src="{{ asset('images/site/viber.svg') }}" alt="viber"></a>
+        <a href="viber://chat?number=+381611609716"><img src="{{ asset('images/site/viber.svg') }}" alt="viber"></a>
         <a href="https://www.google.com/maps/place/ZLATIBOR+DOSTAVA+-+GLOVO+%26+RED+COMBI+DELIVERY,+%D0%97%D0%BB%D0%B0%D1%82%D0%B8%D0%B1%D0%BE%D1%80+31315/data=!4m2!3m1!1s0x47582f4ccd5675b9:0xb38d29aceccf84d1?utm_source=mstt_1&entry=gps"><img src="{{ asset('images/site/G.svg') }}" alt="google+"></a>
     </div>
     <div><a href="https://www.instagram.com/zlatibordostava/"><p style="color: white;">@zlatibordostava</p></a></div>
